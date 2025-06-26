62d97f8c62b6e45fe890ad0fb3b25a3af7a13194
Kirill Safin <safinkirill2003@mail.ru>
2025-06-26 21:23:44 +0500

    FirstProblem

---
+++ FirstProblem/ConsoleApp1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, -8, 2, -3, 7, 1 };
+
+            if (arr.Length < 2)
+            {
+                Console.WriteLine("Массив должен содержать минимум два элемента.");
+                return;
+            }
+
+            FindAnswer(arr);
+        }
+
+        private static void FindAnswer(int[] arr)
+        {
+            int min1 = int.MaxValue;
+            int min2 = int.MaxValue;
+
+            foreach (int num in arr)
+            {
+                if (num <= min1)
+                {
+                    min2 = min1;
+                    min1 = num;
+                }
+                else if (num < min2)
+                {
+                    min2 = num;
+                }
+            }
+
+            Console.WriteLine($"Минимальная пара: ({min1}, {min2})");
+            Console.WriteLine($"Сумма: {min1 + min2}");
+        }
+    }
+}
