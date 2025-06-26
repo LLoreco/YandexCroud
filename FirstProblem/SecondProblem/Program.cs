39b6f967d803c320512a6c954f396e8daaa5b8cf
Kirill Safin <safinkirill2003@mail.ru>
2025-06-26 21:36:25 +0500

    Second Answer

---
+++ FirstProblem/SecondProblem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 3, 5, 9 };
+
+            int sum = 0;
+            foreach (int num in arr)
+                sum += num;
+
+            int powerOfTwo = 1;
+            while (powerOfTwo < sum)
+            {
+                powerOfTwo *= 2;
+            }
+
+            Console.WriteLine($"Сумма элементов: {sum}");
+            Console.WriteLine($"Наименьшая степень двойки большей или равной сумме {sum}: {powerOfTwo}");
+        }
+    }
+}
